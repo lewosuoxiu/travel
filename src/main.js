ba5a44b65143dce5a1e76dfc2de887eac990399f
lewosuoxiu <lewosuoxiu@163.com>
2018-09-13 14:07:00 +0800

    add header

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import './assets/styles/reset.css'
-import './assets/styles/border.css'
-import FastClick from 'fastclick'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import "styles/reset.css";
+import "styles/border.css";
+import FastClick from "fastclick";
+import "styles/iconfont.css";
 
-Vue.config.productionTip = false
-FastClick.attach(document.body)
-
+Vue.config.productionTip = false;
+FastClick.attach(document.body);
 
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
-       hello world
+       <home-header></home-header>
     </div>
 </template>
 
 <script>
-export default{
-  name: 'Home'
-}
+import HomeHeader from "./components/Header";
+export default {
+  name: "Home",
+  components: "Home",
+  components: {
+    HomeHeader
+  }
+};
 </script>
 
 <style>
- 
 </style>
